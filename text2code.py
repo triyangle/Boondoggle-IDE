345951643b4cc6c56d5e8d5e7829192c5d92544a
bradleyq <qu.bradley@gmail.com>
2016-11-12 02:24:00 -0800

    added some stuff

--- text2code.py
+++ text2code.py
@@ -1,1 +1,14 @@
-#text to code stuff here
+#text to array
+def text2arr(s):
+    return s.split()
+
+#error dictionary
+errors =
+    {
+
+    }
+
+def fixtxterror(arr):
+    lst = []
+    [lst.extend([errors[e]]) if e in errors else lst.add(e) for e in arr]
+    return lst
