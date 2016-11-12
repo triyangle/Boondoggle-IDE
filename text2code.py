a812b1b53a81428064b62988845fb2b7010d1535
natetyoung <natetyoung42@gmail.com>
2016-11-12 03:05:28 -0800

    reorganized to statement detection, built more stubs

--- text2code.py
+++ text2code.py
@@ -1,20 +1,28 @@
 #text to code stuff here
+def def_func(arr):
+    #nothing here yet
+def def_var(arr):
+    #nothing here yet
+def call(arr):
+    #nothing here yet
+
+
 def skip(d, *strs):
     for s in strs:
         d[s]=d
 
 def_tree = {
-    'function': 'def_func',
-    'variable': 'def_var'
+    'function': def_func,
+    'variable': def_var
 }
 skip(def_tree, 'a', 'an', 'the')
 
 start_tree = {
     'define': def_tree,
-    'call': 'call'
+    'call': call
 }
 
-def detect_start(arr):
+def detect_statement(arr):
     i = 0
     res = start_tree
     try:
