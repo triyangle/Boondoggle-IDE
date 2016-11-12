1b748184f41270d1a009b3895789a492584280a5
Elliot Stahnke <elliot.stahnke@gmail.com>
2016-11-12 10:06:34 -0800

    Changed to a class implementation

--- speech2text.py
+++ speech2text.py
@@ -6,22 +6,24 @@ import speech_recognition as sr
 from text2code import *
 
 # obtain audio from the microphone
-r = sr.Recognizer()
-while True:
-    with sr.Microphone() as source:
-        r.adjust_for_ambient_noise(source) # listen for 1 second to calibrate the energy threshold for ambient noise levels
-        print("Say something!")
-        audio = r.listen(source)
+class Speech2Text:
+    def __init__(self):
+        self.r = sr.Recognizer()
+    def process(self):
+        with sr.Microphone() as source:
+            self.r.adjust_for_ambient_noise(source) # listen for 1 second to calibrate the energy threshold for ambient noise levels
+            print("Say something!")
+            self.audio = self.r.listen(source)
 
-# recognize speech using Google Speech Recognition
-    try:
-        # for testing purposes, we're just using the default API key
-        # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
-        # instead of `r.recognize_google(audio)`
-        raw_result =  r.recognize_google(audio)
-        print("Google Speech Recognition thinks you said " + raw_result)
-#        gui(convert_to_code(raw_result))
-    except sr.UnknownValueError:
-        print("Google Speech Recognition could not understand audio")
-    except sr.RequestError as e:
-        print("Could not request results from Google Speech Recognition service; {0}".format(e))
+            # recognize speech using Google Speech Recognition
+        try:
+            # for testing purposes, we're just using the default API key
+            # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
+            # instead of `r.recognize_google(audio)`
+            raw_result =  self.r.recognize_google(audio)
+            print("Google Speech Recognition thinks you said " + raw_result)
+            #        gui(convert_to_code(raw_result))
+        except sr.UnknownValueError:
+            print("Google Speech Recognition could not understand audio")
+        except sr.RequestError as e:
+            print("Could not request results from Google Speech Recognition service; {0}".format(e))
