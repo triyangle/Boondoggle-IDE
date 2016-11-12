22a6a8d2614301efd9ef50682032e50cbdb52821
natetyoung <natetyoung42@gmail.com>
2016-11-12 04:43:10 -0800

    Call expressions work now (snapshot of other branch)

--- text2code.py
+++ text2code.py
@@ -5,14 +5,36 @@ def def_func(arr):
 def def_var(arr):
     print()
     #nothing here yet
+
+call_the_func = {'the','a','function'}
+call_on = {'on','the','argument','arguments','parameter','parameters'}
+call_and = {'and', 'also', 'the', 'argument'}
 def call(arr):
-    print()
-    #nothing here yet
+    code = '('
+    i = 0
+    while arr[i] in call_the_func:
+        i+=1
+    n_exp = expression(arr[i:])
+    code += n_exp[0]+' '
+    i += n_exp[1]
+    while arr[i] in call_on:
+        i+=1
+    while arr[i] != 'stop':
+        n_exp = expression(arr[i:])
+        code += n_exp[0]+' '
+        i += n_exp[1]
+        while arr[i] in call_and:
+            i+=1
+    return (code.strip() + ')',i+1)
+    
 
 def skip(d, *strs):
     for s in strs:
         d[s]=d
 
+def all_same(d, val, *keys):
+    for k in keys:
+        d[k] = val
 def_tree = {
     'function': def_func,
     'variable': def_var
@@ -25,18 +47,18 @@ expr_start_tree = {
     'one': '1',
     '1': '1',
     'zero': '0',
-    '0': '0'
+    '0': '0',
 }
+all_same(expr_start_tree,'+','plus','add','+')
 
 def expression(arr):
     i = 0
     res = expr_start_tree
-    try:
-        while isinstance(res, dict):
-            res = res[arr[i]]
-            i += 1
-        if isinstance(res, str):
-            return res
-        return res(arr[i:])
-    except IndexError as e:
-        return arr
+    while isinstance(res, dict):
+        #print('e',arr,i)
+        res = res[arr[i]]
+        i += 1
+    if isinstance(res, str):
+        return (res,1)
+    return res(arr[i:])
+    
