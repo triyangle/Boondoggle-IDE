40912d698cd6829e71f5d615d27d1f45f6929b11
Elliot Stahnke <elliot.stahnke@gmail.com>
2016-11-12 17:37:01 -0800

    Added file management capabilities

--- gui.py
+++ gui.py
@@ -1,9 +1,29 @@
 #gui stuff here
 from tkinter import *
+from tkinter import filedialog
 import tkinter.font as font
 from speech2text import *
 import threading
 
+# Helper functions
+def fill():
+    """A filler function."""
+    print("filled")
+
+def same(f1name, text):
+    """Compares a file to a string and returns TRUE iff they are identical."""
+    if not f1name:
+        return False
+    f = open(f1name, mode='r')
+    contents = f.read().strip()
+    text = text.strip()
+    f.close()
+    if contents != text:
+        return False
+    return True
+
+
+# The main class
 class Application(Frame):
     def __init__(self, master):
         """Creates the structure and functionality of the application."""
@@ -11,17 +31,14 @@ class Application(Frame):
         self.myfont = font.Font(family='Helvetica', size=24) # Customizable font
         self.recording = False
         self.s2t = Speech2Text()
-
+        self.filename = None
+        self.code = ''
+        self.continyu = False
         self.grid()
         self.create_widgets()
 
     def create_widgets(self):
         """Creates and grids all of the necessary widgets for the application."""
-        # Syntax examples:
-        # self.korean_label = Label(self, font = self.myfont)
-        # self.korean_label.grid(row = 0, column = 1, sticky = W)
-        # self.english_button = Button(self, text = "Meaning?", command = self.show_korean, font = self.myfont, underline = 0)
-        # self.english_button.grid(row = 3, column = 0, sticky = W)
         self.record_button = Button(self, text = "Record", command = self.record_toggle, font = self.myfont, underline = 0)
         self.record_button.grid(row=0, column=0, sticky=W)
 
@@ -34,6 +51,26 @@ class Application(Frame):
         self.text = Text(self, width=35, height = 5, font = self.myfont, wrap = WORD)
         self.text.insert(0.0, "Text")
         self.text.grid(row=1, column=0, columnspan = 3, sticky=W)
+
+        self.menubar = Menu(self.master)
+        self.fileMenu = Menu(self.menubar)
+        self.menubar.add_cascade(label="File", menu=self.fileMenu)
+        self.fileMenu.add_command(label="New", command=self.new)
+        self.fileMenu.add_command(label="Open", command=self.open)
+        self.fileMenu.add_command(label="Save", command=self.save)
+        self.fileMenu.add_command(label="Save as", command=self.save_as)
+        self.master.config(menu=self.menubar)
+
+    def create_top_widgets(self):
+        """Creates the widgets for the popout menu."""
+        self.confirm = Toplevel()
+        self.save_button = Button(self.confirm, text="Save", command=self.destroy_save)
+        self.dont_button = Button(self.confirm, text="Don't save", command=self.dont_save)
+        self.cancel_button = Button(self.confirm, text="Cancel", command=self.confirm.destroy)
+
+        self.save_button.grid(row=0, column=0, sticky=W)
+        self.dont_button.grid(row=0, column=1, sticky=W)
+        self.cancel_button.grid(row=0, column=2, sticky=W)
 
     def record_toggle(self):
         """Toggles the recording of speech."""
@@ -51,11 +88,66 @@ class Application(Frame):
         print("Ending thread")
 
     def myprint(self):
+        """Prints the most recent code and inserts it at the cursor."""
         print(self.code)
-        self.text.insert(0.0, self.code)
+        self.text.insert(INSERT, self.code)
 
     def clear(self):
+        """Clears the textbox."""
         self.text.delete(0.0, END)
+
+    def check_save(self):
+        """Checks if the file needs to be saved and handles various cases."""
+        if not same(self.filename, self.text.get(0.0, END)):
+            print(repr(self.text.get(0.0, END)))
+            self.create_top_widgets()
+            self.master.wait_window(self.confirm)
+
+    def new(self):
+        """Creates a new file."""
+        self.continyu = False
+        self.check_save()
+        if self.continyu:
+            self.filename = None
+            self.text.delete(0.0, END)
+        print(self.filename)
+
+    def open(self):
+        """Opens a preexisting file."""
+        self.continyu = False
+        self.check_save()
+        if self.continyu:
+            self.filename = filedialog.askopenfilename(initialdir="./",title="Select file")
+            self.text.delete(0.0, END)
+            with open(self.filename, 'r') as f:
+                self.text.insert(0.0, f.read())
+        print(self.filename)
+
+    def save(self):
+        """Saves the current file."""
+        self.continyu = True
+        if self.filename:
+            with open(self.filename, 'w') as f:
+                f.write(self.text.get(0.0, END))
+            print(self.filename)
+        else:
+            self.save_as()
+
+    def save_as(self):
+        """Saves the current file with a new name."""
+        self.filename = filedialog.asksaveasfilename(initialdir="./", title="Save as...")
+        self.save()
+
+    def destroy_save(self):
+        """Saves the current file and destroys the popout menu."""
+        self.save()
+        self.confirm.destroy()
+
+    def dont_save(self):
+        """Does not save and destroys the popout menu."""
+        self.continyu = True
+        self.confirm.destroy()
+
 
 def run():
     """Starts the program."""
